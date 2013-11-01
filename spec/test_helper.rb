826a8a5d54b6c58962198a43e9937b9255cab94a
Rikke Willer <riwi@dtic.dtu.dk>
2013-11-01 15:43:13 +0100

    Test coverage: Generate rcov for use in CI server

--- spec/test_helper.rb
+++ spec/test_helper.rb
@@ -2,6 +2,16 @@
 ENV['RACK_ENV'] = 'test'
 
 require 'simplecov'
+require 'simplecov-html'
+require 'simplecov-rcov'
+
+class SimpleCov::Formatter::MergedFormatter
+  def format(result)
+     SimpleCov::Formatter::HTMLFormatter.new.format(result)
+     SimpleCov::Formatter::RcovFormatter.new.format(result)
+  end
+end
+SimpleCov.formatter = SimpleCov::Formatter::MergedFormatter
 SimpleCov.start
 
 require 'minitest/autorun'

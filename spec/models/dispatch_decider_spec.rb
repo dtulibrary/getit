dtulibrary/getit

commit cd3eee662be608cbae2a0dd62ba5228f6125b19d

Ensure that for metastore, local access is chosen over remote. Make subtype rule check all results for the given service. Refs #1398.

helpers/rules_helper.rb
@@ -49,18 +49,20 @@ module RulesHelper
   end
 
   def has_seen_service_with_same_subtype(service_name)
-    has_seen_service_with_same_subtype_lambda = lambda do |service_name, service_response|      
-      @status.seen_with_subtype.has_key?(service_name) && service_response.subtype.match(@status.seen_with_subtype[service_name][/[a-z]+_/])
+    has_seen_service_with_same_subtype_lambda = lambda do |service_name, service_response|
+      # compare with each result from the given service whether it has been seen before
+      # only compare beginning of subtype (licence_|openaccess_)
+      @status.seen_with_subtype.has_key?(service_name) && @status.seen_with_subtype[service_name].any? {|subtype| subtype.match(service_response.subtype[/[a-z]+_/]) }
     end
     has_seen_service_with_same_subtype_lambda.curry[service_name]
   end
 
   # returns true if any of the service names has been sent or is onhold
   def has_seen_services(subtype = nil, service_names)
-    has_seen_services_lambda = lambda do |service_names, service_response|            
-      result = service_names.select do |name| 
+    has_seen_services_lambda = lambda do |service_names, service_response|
+      result = service_names.select do |name|
         @status.seen_with_subtype.include?(name) &&
-        (subtype.nil? || @status.seen_with_subtype[name].match(subtype))
+        (subtype.nil? || @status.seen_with_subtype[name].any? {|subtype| subtype.match(service_response.subtype[/[a-z]+_/]) })
       end.length > 0
     end
     has_seen_services_lambda.curry[service_names]
@@ -68,7 +70,7 @@ module RulesHelper
 
   # returns false if any of the services names has not been processed
   def has_not_seen_services(service_names)
-    has_not_processed_services_lambda = lambda do |service_names, service_response|      
+    has_not_processed_services_lambda = lambda do |service_names, service_response|
       !service_names.subset?(Set.new(@status.seen))
     end
     has_not_processed_services_lambda.curry[Set.new(service_names)]

models/dispatch_decider.rb
@@ -76,11 +76,13 @@ class DispatchDecider
     def update(response, status)
       case status
       when :yes
-        @sent[response.source] = response.subtype
+        @sent[response.source] = [] unless @sent.has_key?(response.source)
+        @sent[response.source] << response.subtype
         @responses << response
         @count += 1
       when :maybe
-        @onhold[response.source] = response.subtype
+        @onhold[response.source] = [] unless @onhold.has_key?(response.source)
+        @onhold[response.source]<< response.subtype
       else
         if response.kind_of?(ServiceResponse)
           @ignore << response.source

models/metastore.rb
@@ -27,9 +27,15 @@ class Metastore
       case @category
       when "fulltext"        
         
+        responses = []
         metastore_response["docs"].first[key].each do |f|          
-          service_responses << metastore_fulltext_response(JSON.parse(f))
+          responses << metastore_fulltext_response(JSON.parse(f))
         end
+
+        sr_licensed, sr_openaccess = responses.partition {|sr| sr.subtype.match("license") }
+        # sort on subtype - pick local over remote
+        service_responses << sr_licensed.sort_by(&:subtype).first unless sr_licensed.empty?
+        service_responses << sr_openaccess.first unless sr_openaccess.empty?
 
       when "alis"
         response = metastore_service_response
@@ -62,8 +68,13 @@ class Metastore
     service_responses
   end    
   
-  def get_query    
-    {"q" => "{!raw f=cluster_id_ss v=$id}", "id" => "#{@reference.custom_co_data["id"] || nil}", "fl" => "#{metastore_key}", "wt" => "json"}
+  def get_query
+    {
+      "q" => "{!raw f=cluster_id_ss v=$id}", "id" => "#{@reference.custom_co_data["id"] || nil}", 
+      "fl" => "#{metastore_key}", 
+      "fq" => "access_ss:#{@reference.dtu? ? 'dtu' : 'dtupub'}",
+      "wt" => "json"
+    }
   end
 
   def metastore_key     

models/rules.rb
@@ -29,7 +29,7 @@ module Rules
 
     rule :nal_if_no_open_access,
          reply: service_is_not("nal"),
-         skip: has_seen_services("openaccess", ["metastore", "sfx"]),       
+         skip: has_seen_services("openaccess", ["metastore", "sfx"]),
          wait: has_not_seen_services(["metastore", "sfx"])
 
     fulltext_common_rules
@@ -79,7 +79,7 @@ module Rules
   def fulltext_common_rules
 
     # metastore has higher priority than sfx for the same subtypes (licensed or open access)
-    rule :metastore_preempts_sfx,   
+    rule :metastore_preempts_sfx,
          reply: service_is_not("sfx"),
          skip: has_seen_service_with_same_subtype("metastore"),
          # wait for decision if we haven't seen any responses from metastore yet

spec/models/metastore_spec.rb
@@ -19,82 +19,83 @@ describe Metastore do
       "req_id"      => "dtu_staff"
     }
 
-    configuration = {"url" => "http://example.com", "category" => "fulltext", "service_type" => "fulltext"}    
+    configuration = {"url" => "http://example.com", "category" => "fulltext", "service_type" => "fulltext", "dtic_url" => "http://example.com/"}
 
     it "fetches a fulltext url" do
 
-      EM.run_block {
+      EM.run_block do
         reference = Reference.new(params)
         stub_request(:get, /#{configuration['url']}.*/).to_return(File.new("spec/fixtures/solr1.txt"))
         metastore = Metastore.new(reference, configuration)
-        metastore.callback { |result|     
+        metastore.callback do |result|     
           result.first.url.must_equal("http://arxiv.org/abs/0801.1253")
-          result.first.service_type.must_equal("fulltext")          
-        }
-        metastore.errback { |error| 
+          result.first.service_type.must_equal("fulltext")
+        end
+        metastore.errback do |error| 
           flunk error
-        }
-      }
+        end
+      end
+    end
+
+    it "choses a local fulltext url over an external" do
+
+      EM.run_block do
+        reference = Reference.new(params)
+        stub_request(:get, /#{configuration['url']}.*/).to_return(File.new("spec/fixtures/solr6.txt"))
+        metastore = Metastore.new(reference, configuration)
+        metastore.callback do |result|     
+          result.first.url.must_equal("#{configuration['dtic_url']}cup?pi=%2Fs0266%2F4674%2F0999%2F0514.pdf&key=140017028")
+          result.first.service_type.must_equal("fulltext")
+          result.length.must_equal 1
+        end
+        metastore.errback do |error| 
+          flunk error
+        end
+      end
     end
 
     it "has no fulltext list" do
 
-      EM.run_block {
+      EM.run_block do
         reference = Reference.new(params)
         stub_request(:get, /#{configuration['url']}.*/).to_return(File.new("spec/fixtures/solr2.txt"))
         metastore = Metastore.new(reference, configuration)
-        metastore.callback { |result|        
-          result.must_be_empty
-        }
-        metastore.errback { |error| 
-          flunk error
-        }      
-      }
+        metastore.callback { |result| result.must_be_empty }
+        metastore.errback { |error| flunk error }
+      end
     end
 
-    it "does not exists" do    
+    it "does not exists" do
 
-      EM.run_block {
+      EM.run_block do
         reference = Reference.new(params)
         stub_request(:get, /#{configuration['url']}.*/).to_return(File.new("spec/fixtures/solr3.txt"))
         metastore = Metastore.new(reference, configuration)
-        metastore.callback { |result|        
-          result.must_be_empty
-        }
-        metastore.errback { |error| 
-          flunk error
-        }      
-      }
+        metastore.callback { |result| result.must_be_empty }
+        metastore.errback { |error| flunk error }
+      end
     end
 
     it "points at wrong server" do
 
-      EM.run_block {
+      EM.run_block do
         reference = Reference.new(params)
         stub_request(:get, /#{configuration['url']}.*/).to_return(:status => 404)
         metastore = Metastore.new(reference, configuration)
-        metastore.callback { |result|        
-          result.must_be_empty
-        }
-        metastore.errback { |error| 
-          error.must_match /^Service Metastore failed with status 404*/
-        }
-      }
+        metastore.callback { |result| result.must_be_empty }
+        metastore.errback { |error| error.must_match /^Service Metastore failed with status 404/ }
+      end
     end
 
     it "skips references that should be excluded" do
 
-      EM.run_block {        
+      EM.run_block do
         reference = Reference.new(params.merge({"rft.date" => "2013", "rft.issn" => "01443577", "rft_id" => "urn:issn:01443577"}))
         stub_request(:get, /#{configuration['url']}.*/).to_return(File.new("spec/fixtures/solr1.txt"))
         metastore = Metastore.new(reference, configuration)
-        metastore.callback { |result|     
-          result.must_be_empty          
-        }
-        metastore.errback { |error| 
-          flunk error
-        }
-      }
+        metastore.callback { |result| result.must_be_empty }
+        metastore.errback { |error| flunk error }
+      end
     end
   end
 
@@ -122,17 +123,15 @@ describe Metastore do
 
     it "fetches an alis url" do
 
-      EM.run_block {
+      EM.run_block do
         stub_request(:get, /#{configuration['url']}.*/).to_return(File.new("spec/fixtures/solr4.txt"))
         metastore = Metastore.new(reference, configuration)
-        metastore.callback { |result|        
+        metastore.callback do |result|
           result.first.url.must_equal("http://example.com?id=000441656")
-          result.first.service_type.must_equal("fulltext")     
-        }
-        metastore.errback { |error| 
-          flunk error
-        }
-      }
+          result.first.service_type.must_equal("fulltext")
+        end
+        metastore.errback { |error| flunk error }
+      end
     end
   end
 
@@ -157,17 +156,15 @@ describe Metastore do
     reference = Reference.new(params)
 
     it "fetches the holdings" do
-      EM.run_block {
+      EM.run_block do
         stub_request(:get, /#{configuration['url']}.*/).to_return(File.new("spec/fixtures/solr5.txt"))
         metastore = Metastore.new(reference, configuration)
-        metastore.callback { |result|        
-          result.first.holdings_list.length.must_equal 3          
+        metastore.callback do |result|
+          result.first.holdings_list.length.must_equal 3
           result.first.service_type.must_equal "fulltext"
-        }
-        metastore.errback { |error| 
-          flunk error
-        }
-      }      
+        end
+        metastore.errback { |error| flunk error }
+      end
     end
   end
 end

commit 192b7ec6a7ed03481ffc701dbf075d7cf4a52792

Fix error in NAL rule evaluation introduced in rev cd3eee66, refs #198

helpers/rules_helper.rb
@@ -52,7 +52,7 @@ module RulesHelper
     has_seen_service_with_same_subtype_lambda = lambda do |service_name, service_response|
       # compare with each result from the given service whether it has been seen before
       # only compare beginning of subtype (licence_|openaccess_)
-      @status.seen_with_subtype.has_key?(service_name) && @status.seen_with_subtype[service_name].any? {|subtype| subtype.match(service_response.subtype[/[a-z]+_/]) }
+      @status.seen_with_subtype.has_key?(service_name) && @status.seen_with_subtype[service_name].any? {|subtype| subtype.match(service_response.subtype[/[a-z]+_?/]) }
     end
     has_seen_service_with_same_subtype_lambda.curry[service_name]
   end
@@ -62,7 +62,7 @@ module RulesHelper
     has_seen_services_lambda = lambda do |service_names, service_response|
       result = service_names.select do |name|
         @status.seen_with_subtype.include?(name) &&
-        (subtype.nil? || @status.seen_with_subtype[name].any? {|subtype| subtype.match(service_response.subtype[/[a-z]+_/]) })
+        (subtype.nil? || @status.seen_with_subtype[name].any? {|subtype| subtype.match(service_response.subtype[/[a-z]+_?/]) })
       end.length > 0
     end
     has_seen_services_lambda.curry[service_names]

spec/models/dispatch_decider_spec.rb
@@ -108,4 +108,33 @@ describe DispatchDecider do
     sfx_can_send = dd.can_send(sfx_rep)
     sfx_can_send.must_equal(:no)
   end
+
+  it "shows nal if no open access" do
+    reference = Reference.new(params.merge({"req_id" => "anonymous"}))
+    dd = DispatchDecider.new("fulltext", reference)
+
+    metastore_rep = ServiceResponse.new
+    metastore_rep.service_type = "fulltext"
+    metastore_rep.subtype = "license_local"
+    metastore_rep.source = "metastore"
+
+    sfx_rep = ServiceResponse.new
+    sfx_rep.service_type = "fulltext"
+    sfx_rep.subtype = "license_remote"
+    sfx_rep.source = "sfx"
+
+    nal_rep = NalServiceResponse.new
+    nal_rep.service_type = "fulltext"
+    nal_rep.subtype = "nal"
+    nal_rep.source = "nal"
+
+    metastore_can_send = dd.can_send(metastore_rep)
+    metastore_can_send.must_equal(:yes)
+    dd.status.update(metastore_rep, metastore_can_send)
+    sfx_can_send = dd.can_send(sfx_rep)
+    sfx_can_send.must_equal(:no)
+    dd.status.update(sfx_rep, sfx_can_send)
+    nal_can_send = dd.can_send(nal_rep)
+    nal_can_send.must_equal(:yes)    
+  end
 end
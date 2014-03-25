a6c261afecad7fad155cbfb7a9705686f2773375
Rikke Willer <riwi@dtic.dtu.dk>
2014-03-25 13:23:24 +0100

    Various text changes, refs #1568

--- models/fulltext_service_response.rb
+++ models/fulltext_service_response.rb
@@ -11,25 +11,27 @@ class FulltextServiceResponse < ServiceResponse
   attr_accessor :tool_tip
   attr_accessor :icon
   attr_accessor :holdings_list
+  attr_accessor :list_text
 
   def initialize
     super
-    @public_vars.concat(["@short_name", "@type", "@short_explanation", "@lead_text", "@explanation", 
-      "@button_text", "@tool_tip", "@icon", "@holdings_list"])
+    @public_vars.concat(["@short_name", "@type", "@short_explanation", "@lead_text", "@explanation",
+      "@button_text", "@tool_tip", "@icon", "@holdings_list", "@list_text"])
     @holdings_list = Array.new
   end
 
-  def set_translations(doc_type, sub_type, user_type) 
+  def set_translations(doc_type, sub_type, user_type)
     lookup_text = "fulltext.#{doc_type}.#{sub_type}.%s.#{user_type}"
 
     set_translation("short_name", lookup_text)
-    set_translation("type", lookup_text) 
+    set_translation("type", lookup_text)
     set_translation("short_explanation",  lookup_text)
     set_translation("lead_text",  lookup_text)
     set_translation("explanation",  lookup_text)
     set_translation("button_text",  lookup_text)
     set_translation("tool_tip",  lookup_text)
     set_translation("icon",  lookup_text)
+    set_translation("list_text",  lookup_text)
   end
 
 end

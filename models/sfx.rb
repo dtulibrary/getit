e2bccbaa3cfb1c0655817df5edd21e728567de93
Rikke Willer <riwi@dtic.dtu.dk>
2014-03-27 12:15:41 +0100

    Fix buggy commit b740507

--- models/sfx.rb
+++ models/sfx.rb
@@ -182,10 +182,11 @@ class Sfx
       standard_numbers.sort.map do |number, type|
         query_params = params.dup
         query_params << "rft.#{type.to_s}=#{number}"
-        query_params << "fromfindit=true"
         queries << URI.escape(query_params.join('&'))
       end
     end
+
+    queries.map! {|query| "#{query}&fromfindit=true"}
 
     queries
   end
